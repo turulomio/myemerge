7df50c7c1f8a7e9128b72c535d198cb9a7fd787b
turulomio <turulomio@yahoo.es>
2021-11-22 12:26:35 +0100

    Removed config code. Added CCACHE_DIR to all script to use it one set in make.conf

--- myemerge/myemerge_sync.py
+++ myemerge/myemerge_sync.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 from myemerge.gettext import _
 from myemerge.objects.command import command
-from myemerge.myconfigparser import MyConfigParser
 from myemerge.version import __version__, __versiondate__
 from os import environ, system
 from sys import exit
@@ -15,12 +14,11 @@ def main():
     parser.add_argument('--noclean', help=_("Do note cleans innecesary packages after compilation"),  action='store_true',  default=False)
 
     args=parser.parse_args()
-    config=MyConfigParser('/etc/myemerge/myemerge.ini')
 
 
+    environ["CCACHE_DIR"]="/var/cache/ccache" #CCache path of portage
     ##ccache is only used if it's set in /etc/portage/make.conf
     if args.ccache_stats==True:
-        environ["CCACHE_DIR"]="/var/cache/ccache" #CCache path of portage
         if args.ccache_stats==True:
             system("ccache -s")
             exit(0)
@@ -38,4 +36,3 @@ def main():
         command("emerge -v  @preserved-rebuild --keep-going y")
         if args.noclean is False:
             command("eclean-dist -d")
-    config.save()
