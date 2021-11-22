43c7c8cd84412b56b5f7d72badc77fc40f00c31b
turulomio <turulomio@yahoo.es>
2021-11-22 12:22:45 +0100

    Added --nosync and --noclean parameters

--- myemerge/myemerge_sync.py
+++ myemerge/myemerge_sync.py
@@ -2,7 +2,6 @@ from argparse import ArgumentParser
 from myemerge.gettext import _
 from myemerge.objects.command import command
 from myemerge.myconfigparser import MyConfigParser
-from myemerge.cpupower import sys_set_cpu_max_scaling_freq, sys_get_cpu_max_scaling_freq, sys_get_cpu_max_freq, is_cpufreq_configured
 from myemerge.version import __version__, __versiondate__
 from os import environ, system
 from sys import exit
@@ -10,34 +9,21 @@ from sys import exit
 def main():
     parser=ArgumentParser(description=_("My method to update my Gentoo System"))
     parser.add_argument('--version', action='version', version="{} ({})".format(__version__, __versiondate__))
-    parser.add_argument('--config', help=_("Write a config file in /etc/myemerge/myemerge.ini"),  action='store_true',  default=False)
     parser.add_argument('--rebuild', help=_("Rebuild the whole Gentoo system"),  action='store_true',  default=False)
     parser.add_argument('--ccache_stats', help=_("Shows ccache statistics"),  action='store_true',  default=False)
+    parser.add_argument('--nosync', help=_("Do note makes emerge --sync"),  action='store_true',  default=False)
+    parser.add_argument('--noclean', help=_("Do note cleans innecesary packages after compilation"),  action='store_true',  default=False)
 
     args=parser.parse_args()
     config=MyConfigParser('/etc/myemerge/myemerge.ini')
 
+
+    ##ccache is only used if it's set in /etc/portage/make.conf
     if args.ccache_stats==True:
         environ["CCACHE_DIR"]="/var/cache/ccache" #CCache path of portage
         if args.ccache_stats==True:
             system("ccache -s")
             exit(0)
-
-
-    if args.config==True: #Writes a config file
-        config.save()
-        print("You must set your settings in /etc/myemerge/myemerge.ini. Use man myemerge for help.")
-        exit(0)
-    
-    if is_cpufreq_configured():
-        cpu_hz_before=sys_get_cpu_max_scaling_freq()
-        cpu_hz=config.get('cpupower','cpu_hz',  str(sys_get_cpu_max_freq()))
-        sys_set_cpu_max_scaling_freq(int(cpu_hz))
-        
-    if args.config==True: #Writes a config file
-        config.save()
-        print("You must set your settings in /etc/myemerge/myemerge.ini. Use man myemerge for help.")
-        exit(0)
 
     if args.rebuild==True:
         command("emaint cleanresume -f")
@@ -45,13 +31,11 @@ def main():
         command("emerge @preserved-rebuild")
         command("revdep-rebuild -i -- --keep-going y")
     else:
-        command("emerge --sync")
+        if args.nosync is False:
+            command("emerge --sync")
         command("emaint cleanresume -f")
         command("emerge --update --newuse --deep -v --keep-going y @world --backtrack=60 --with-bdeps y --changed-deps y")
         command("emerge -v  @preserved-rebuild --keep-going y")
-        command("eclean-dist -d")
-
-    if is_cpufreq_configured():
-        sys_set_cpu_max_scaling_freq(cpu_hz_before)
-
+        if args.noclean is False:
+            command("eclean-dist -d")
     config.save()
