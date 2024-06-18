800663eb40400930a6671caa1e688b0804171f60
turulomio <turulomio@yahoo.es>
2024-06-18 06:59:18 +0200

    Translate works

--- myemerge/poethepoet.py
+++ myemerge/poethepoet.py
@@ -1,7 +1,6 @@
 from myemerge import __version__
 from os import system
 from myemerge.reusing.github import download_from_github
-from myemerge.reusing.file_functions import replace_in_file
 from myemerge.gettext import _
 from sys import argv
 
@@ -12,7 +11,7 @@ def release():
     print(_("  * Change version in pyproject.toml"))
     print(_("  * Edit Changelog in README"))
     print("  * poe translate")
-    print("  * mcedit locale/es.po")
+    print("  * mcedit myemerge/locale/es.po")
     print("  * poe translate")
     print("  * git commit -a -m 'myemerge-{}'".format(__version__))
     print("  * git push")
@@ -21,11 +20,11 @@ def release():
     print(_("  * Upload to portage repository")) 
 
 def translate():
-        system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o locale/myemerge.pot *.py myemerge/*.py myemerge/objects/*.py")
-        system("msgmerge -N --no-wrap -U locale/es.po locale/myemerge.pot")
-        system("msgmerge -N --no-wrap -U locale/fr.po locale/myemerge.pot")
-        system("msgfmt -cv -o myemerge/locale/es/LC_MESSAGES/myemerge.mo locale/es.po")
-        system("msgfmt -cv -o myemerge/locale/fr/LC_MESSAGES/myemerge.mo locale/fr.po")
+        system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o myemerge/locale/myemerge.pot myemerge/*.py myemerge/objects/*.py")
+        system("msgmerge -N --no-wrap -U myemerge/locale/es.po myemerge/locale/myemerge.pot")
+        system("msgmerge -N --no-wrap -U myemerge/locale/fr.po myemerge/locale/myemerge.pot")
+        system("msgfmt -cv -o myemerge/locale/es/LC_MESSAGES/myemerge.mo myemerge/locale/es.po")
+        system("msgfmt -cv -o myemerge/locale/fr/LC_MESSAGES/myemerge.mo myemerge/locale/fr.po")
 
 def reusing():
     """
@@ -38,14 +37,9 @@ def reusing():
         local=True
         print("Update code in local without downloading was selected with --local")
     if local is False:
-        download_from_github('turulomio','reusingcode','python/casts.py', 'myemerge/reusing')
         download_from_github('turulomio','reusingcode','python/github.py', 'myemerge/reusing')
         download_from_github('turulomio','reusingcode','python/cpupower.py', 'myemerge/reusing')
-        download_from_github('turulomio','reusingcode','python/datetime_functions.py', 'myemerge/reusing')
         download_from_github('turulomio','reusingcode','python/myconfigparser.py', 'myemerge/reusing')
         download_from_github('turulomio','reusingcode','python/github.py', 'myemerge/reusing/')
         download_from_github('turulomio','reusingcode','python/file_functions.py', 'myemerge/reusing/')
-        
-    replace_in_file("myemerge/reusing/myconfigparser.py",  "from casts",  "from .casts")
-    replace_in_file("myemerge/reusing/myconfigparser.py",  "from datetime_functions",  "from .datetime_functions")
 
